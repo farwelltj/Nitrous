60439cb9419e755cb59f60d6381ec602c62c9076
farwelltj <farwelltj1988@gmail.com>
2013-11-24 15:14:47 +0000

    somewhat working app

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
-
-#@app.route('/hello/')
-#def hello(name=None):
-#   return render_template('hello.html', name=name)
-
 @app.route('/')
 def index():
     return redirect(url_for('static', filename='index.html'))
