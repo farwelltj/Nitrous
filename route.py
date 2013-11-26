a34e571b1d6ce165c9aeac5663499149dd7d44af
farwelltj <farwelltj1988@gmail.com>
2013-11-26 15:30:13 +0000

    more flask turtorial and css fixed

--- route.py
+++ route.py
@@ -1,8 +1,8 @@
 # all the imports
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
-from flask import Flask, render_template
+from flask import Flask, request, session, g, redirect, url_for, \abort, render_template, flash
+from contextlib import closing
+from flaskr import init_db
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -16,6 +16,12 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+  
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
   
 @app.route('/')
 def home():
