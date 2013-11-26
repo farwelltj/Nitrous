e06ac9023d46bbf6b8f8e5f31e4a24babfec1fce
farwelltj <farwelltj1988@gmail.com>
2013-11-26 15:39:55 +0000

    redid the app.py

--- route.py
+++ route.py
@@ -1,27 +1,6 @@
-# all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \abort, render_template, flash
-from contextlib import closing
-from flaskr import init_db
-
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-app = Flask(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+from flask import Flask, render_template
   
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+app = Flask(__name__)
   
 @app.route('/')
 def home():
