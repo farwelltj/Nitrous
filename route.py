f5b273fc4a68721d14137b7e6d6a8c6fda6f7751
farwelltj <farwelltj1988@gmail.com>
2013-11-26 15:15:48 +0000

    flask turtorial

--- route.py
+++ route.py
@@ -1,14 +1,29 @@
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
 from flask import Flask, render_template
-  
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
   
 @app.route('/')
 def home():
   return render_template('home.html')
   
-#@app.route('/about')
-#def about():
-#  return render_template('about.html')
+@app.route('/about')
+def about():
+  return render_template('about.html')
   
 if __name__ == '__main__':
   app.run(debug=True)
